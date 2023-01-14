46acc88e97bcf6fb519b520f271b4e0fc1de55a0
weqhey <weqhey@gmail.com>
2023-01-14 11:55:51 +0200

    Add examples of multithreading

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SupermarketEF.EF;
 using SupermarketEF.Models;
-/*
-Навести приклади використання union, except, intersect, join, distinct, group by, агрегатних функцій.  
-*/
-
-GroupBy();
-Count();
 
 void Create()
 {
@@ -126,7 +120,7 @@ void Union()
 {
     SupermarketContext context = new SupermarketContext();
     var query = context.Workers.Select(p => p.Id).Union(context.Receipts.Select(p => p.WorkerId));
-    foreach(var it in query)
+    foreach (var it in query)
     {
         Console.WriteLine(it);
     }
@@ -136,7 +130,7 @@ void Except()
 {
     SupermarketContext context = new SupermarketContext();
     var query = context.Workers.Select(p => p.Id).Except(context.Receipts.Select(p => p.WorkerId));
-    foreach(var it in query)
+    foreach (var it in query)
     {
         Console.WriteLine(it);
     }
@@ -146,7 +140,7 @@ void Intersect()
 {
     SupermarketContext context = new SupermarketContext();
     var query = context.Workers.Select(p => p.Id).Intersect(context.Receipts.Select(p => p.WorkerId));
-    foreach(var it in query)
+    foreach (var it in query)
     {
         Console.WriteLine(it);
     }
@@ -154,7 +148,12 @@ void Intersect()
 
 void Distinct()
 {
-
+    SupermarketContext context = new SupermarketContext();
+    var query = context.Products.Select(p => p.Price).Distinct();
+    foreach (var it in query)
+    {
+        Console.WriteLine(it);
+    }
 }
 
 void Join()
@@ -164,12 +163,13 @@ void Join()
         context.ProductInReceipts,
         product => product.Id,
         productInReceipt => productInReceipt.ProductId,
-        (product, productInReceipt) => new { 
+        (product, productInReceipt) => new
+        {
             ProductInReceiptId = productInReceipt.ProductId,
             ProductId = product.Id
         }
     );
-    foreach(var it in query)
+    foreach (var it in query)
     {
         Console.WriteLine(it);
     }
@@ -182,17 +182,18 @@ void GroupBy()
         context.Workers,
         receipt => receipt.WorkerId,
         worker => worker.Id,
-        (receipt, worker) => new { 
+        (receipt, worker) => new
+        {
             WorkerId = worker.Id,
             ReceiptId = receipt.Id
-        }) 
+        })
         .GroupBy(p => p.WorkerId)
         .Select(m => new
         {
             m.Key,
             Count = m.Count()
         });
-    foreach(var it in query)
+    foreach (var it in query)
     {
         Console.WriteLine(it);
     }
@@ -203,4 +204,118 @@ void Count()
     SupermarketContext context = new SupermarketContext();
     var query = context.Workers.Count();
     Console.WriteLine(query);
+}
+
+async Task AddAsync()
+{
+    SupermarketContext context = new SupermarketContext();
+    for (int i = 0; i < 10; i++)
+    {
+        await context.Workers.AddAsync(new Worker
+        {
+            Name = i.ToString(),
+            Position = i.ToString()
+        });
+        await context.SaveChangesAsync();
+    }
+}
+async Task ReadAsync()
+{
+    SupermarketContext context = new SupermarketContext();
+    var list = await context.Workers.ToListAsync();
+    foreach (var it in list)
+    {
+        Console.WriteLine(it.Name);
+    }
+}
+void LockExample()
+{
+    SupermarketContext context = new SupermarketContext();
+    object locker = new object();
+    int cnt = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        Thread newThread = new(() =>
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                lock (locker)
+                {
+                    context.Workers.Add(new Worker
+                    {
+                        Name = cnt.ToString(),
+                        Position = cnt.ToString()
+                    });
+                    cnt++;
+                    Console.WriteLine(cnt);
+                    context.SaveChanges();
+                }
+            }
+        });
+        newThread.Start();
+        Thread.Sleep(100);
+    }
+}
+
+void MonitorExample()
+{
+    SupermarketContext context = new SupermarketContext();
+    object locker = new object();
+    int cnt = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        Thread newThread = new(() =>
+        {
+            for(int j = 0; j < 100; j++)
+            {
+                bool lockTaken = false;
+                try
+                {
+                    Monitor.Enter(locker, ref lockTaken);
+                    context.Workers.Add(new Worker
+                    {
+                        Name = cnt.ToString(),
+                        Position = cnt.ToString()
+                    });
+                    cnt++;
+                    Console.WriteLine(cnt);
+                    context.SaveChanges();
+                }
+                finally
+                {
+                    if (lockTaken) Monitor.Exit(locker);
+                }
+            }
+        });
+        newThread.Start();
+        Thread.Sleep(100);
+    }
+}
+
+void MutexExample()
+{
+    SupermarketContext context = new SupermarketContext();
+    Mutex mutexObj = new Mutex();
+    int cnt = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        Thread newThread = new(() =>
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                mutexObj.WaitOne();
+                context.Workers.Add(new Worker
+                {
+                    Name = cnt.ToString(),
+                    Position = cnt.ToString()
+                });
+                context.SaveChanges();
+                cnt++;
+                Console.WriteLine(cnt);
+                mutexObj.ReleaseMutex();
+            }
+        });
+        newThread.Start();
+        Thread.Sleep(1000);
+    }
 }
